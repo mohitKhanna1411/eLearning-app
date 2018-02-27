e153460309535c12183d9048553e818db7a4df67
adityaachaubey <adityaa.chaubey@gmail.com>
2018-02-27 19:40:54 +0530

    presigned url on image view tested

--- platforms/browser/www/assets/js/angular-files/scriptLogin.js
+++ platforms/browser/www/assets/js/angular-files/scriptLogin.js
@@ -1,6 +1,6 @@
-var mainPhoneGapApp = angular.module('mainPhoneGapApp', []);
+var mainPhoneGapApp = angular.module('mainPhoneGapApp', ['ngStorage']);
 
-mainPhoneGapApp.controller('loginController', function ($scope, $http) {
+mainPhoneGapApp.controller('loginController',['$scope','$http','$window','$localStorage', function ($scope, $http,$window,$localStorage) {
 
 
     $scope.loginDoctor = function() {
@@ -8,7 +8,12 @@ mainPhoneGapApp.controller('loginController', function ($scope, $http) {
         $http.post('http://ec2-13-126-237-55.ap-south-1.compute.amazonaws.com:3000/api/chironx/authenticate/login',loginData).success(function(res){
 
             console.log(res);
+            //$scope.token=res.token;
+            $localStorage.token=res.token;
+            console.log($localStorage.token);
+            // localStorage.setItem("token",res.token);
+            $window.location.href="mainDashboard.html";
         })
     }
 
-});
+}]);
