541c32caebd50c1a868c1f23826e018bbf8eb7bd
ChironMohit <mohitkhanna@adveniotecnosys.com>
2018-02-27 17:19:27 +0530

    testing phase

--- www/assets/js/angular-files/scriptRegistration.js
+++ www/assets/js/angular-files/scriptRegistration.js
@@ -3,9 +3,12 @@ var mainPhoneGapApp = angular.module('mainPhoneGapApp', []);
 mainPhoneGapApp.controller('registrationController', function ($scope, $http) {
 
 
-    $scope.submitTest = function() {
-
-    $http.get('http://chironapi.chironx.cloud/api/chironx/status').success(function(res){
+    $scope.submitRegistrationForm = function() {
+        var registerData = {
+            email : $scope.email ,
+            hashedPassword : $scope.
+        }
+    $http.post('http://ec2-13-126-237-55.ap-south-1.compute.amazonaws.com:3000/api/chironx/authenticate/register').success(function(res){
 
         console.log(res);
     })
