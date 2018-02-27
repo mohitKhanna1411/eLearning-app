0e79097725e3083db1476f8d0c4facba0bcf454e
ChironMohit <mohitkhanna@adveniotecnosys.com>
2018-02-27 17:35:04 +0530

    pending test

--- platforms/browser/www/assets/js/angular-files/scriptRegistration.js
+++ platforms/browser/www/assets/js/angular-files/scriptRegistration.js
@@ -6,7 +6,16 @@ mainPhoneGapApp.controller('registrationController', function ($scope, $http) {
     $scope.submitRegistrationForm = function() {
         var registerData = {
             email : $scope.email ,
-            hashedPassword : $scope.
+            hashedPassword : $scope.password ,
+            Login_ID : $scope.username,
+            isAdmin : false ,
+            Doc_Name : $scope.username ,
+            Doc_Institution : $scope.institution ,
+            Doc_Experience  : $scope.experience,
+            Doc_Expert : $scope.expert,
+            isActive  :false ,
+            Registration_Date : Date
+
         }
     $http.post('http://ec2-13-126-237-55.ap-south-1.compute.amazonaws.com:3000/api/chironx/authenticate/register').success(function(res){
 
