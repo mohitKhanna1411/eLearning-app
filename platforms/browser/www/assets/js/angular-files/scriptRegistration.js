8dd0ad426eb1eb47629bffd7230394f9a5c5b645
ChironMohit <mohitkhanna@adveniotecnosys.com>
2018-02-27 14:04:00 +0530

    angular basic setup done

---
+++ platforms/browser/www/assets/js/angular-files/scriptRegistration.js
@@ -0,0 +1,10 @@
+var mainApp = angular.module('mainPhoneGapApp', []);
+
+mainApp.controller('registrationController', function ($scope, $http) {
+
+
+    $scope.submitTest = function() {
+
+
+    }
+});
