13b1e3c04d6a40949b15a358d3fc5456f08e0dde
PulkitG <pulkit@chironx.ai>
2018-03-27 23:23:33 +0530

    all stages data save function

--- www/assets/js/angular-files/imageLoader.js
+++ www/assets/js/angular-files/imageLoader.js
@@ -1,5 +1,5 @@
-var mainPhoneGapApp = angular.module('mainPhoneGapApp', ['ngStorage','angular-growl']);
-mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localStorage','growl', function ($scope, $http, $localStorage,growl) {
+var mainPhoneGapApp = angular.module('mainPhoneGapApp', ['ngStorage', 'angular-growl']);
+mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localStorage', 'growl', '$compile','$window', function ($scope, $http, $localStorage, growl, $compile, $window) {
     var config = {
         headers: {
             "Authorization": "Bearer " + $localStorage.token
@@ -7,84 +7,85 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
     };
     console.log($localStorage.token);
 
-    
+
     // Imageviewer
-    function imageView(image){
-     
+    function imageView(image) {
+
         var curImageIdx = 1,
             total = image.length;
         var wrapper = $('#image-gallery'),
             curSpan = wrapper.find('.current');
         var viewer = ImageViewer(wrapper.find('.image-container'));
-     
-        function showImage(){
+
+        function showImage() {
             var imgObj = image[curImageIdx - 1];
             viewer.load(imgObj.small, imgObj.big);
             curSpan.html(curImageIdx);
         }
-     
+
         //initially show image
         showImage();
-    
+
     }
 
-    setDefaultImage = function()
-    {
-        var images=[];
-        var imageArr={};
-    
-              imageArr={
-                small : './assets/img/chironzetalogo.png',
-                big : './assets/img/chironzetalogo.png'
-    
-              };
-             images.push(imageArr);
-    
-          console.log(images);
-          imageView(images);
+    // Setting default image when completed / images not present
+    setDefaultImage = function () {
+        var images = [];
+        var imageArr = {};
+
+        imageArr = {
+            small: './assets/img/chironzetalogo.png',
+            big: './assets/img/chironzetalogo.png'
+
+        };
+        images.push(imageArr);
+
+        console.log(images);
+        imageView(images);
     }
 
 
     // Get random image
-    $http.get('http://192.168.0.109:8000/api/chironx/assign/random/image', config).success(function (res) {
+    getRandomImage = function () {
+        // Get random image
+        $http.get('http://192.168.0.109:8000/api/chironx/assign/random/image', config).success(function (res) {
 
-        console.log(res);
+            console.log(res);
 
-        if(res.image === 0)
-        {
-            growl.success('All current images have been marked!',{title: 'Success'});
-            $("#test *").attr("disabled", "disabled").off('click');
-            setDefaultImage();
-        }
-        else
-        {
-            $scope.imageObject = res.image;
-            // $scope.image_src = "assets/img/test_image.jpg";
-            console.log("Done loading");
-            $scope.image_src = res.presignedUrl;
-    
-            var images=[];
-            var imageArr={};
-        
-                  imageArr={
-                    small : res.presignedUrl,
-                    big : res.presignedUrl
-        
-                  };
-                 images.push(imageArr);
-        
-              console.log(images);
-              imageView(images);
-        }
-        
+            if (res.image === 0) {
+                growl.success('All current images have been marked!', { title: 'Success' });
+                $("#test *").attr("disabled", "disabled").off('click');
+                setDefaultImage();
+            }
+            else {
+                $scope.imageObject = res.image;
+                // $scope.image_src = "assets/img/test_image.jpg";
+                console.log("Done loading");
+                $scope.image_src = res.presignedUrl;
 
-    })
-        
+                var images = [];
+                var imageArr = {};
+
+                imageArr = {
+                    small: res.presignedUrl,
+                    big: res.presignedUrl
+
+                };
+                images.push(imageArr);
+
+                //   console.log(images);
+                imageView(images);
+            }
+
+
+        });
+    }
+
 
 
     // Get the list of clinical features
-        $http.get('http://192.168.0.109:8000/api/chironx/list/diagnosis', config).success(function (res) {
-        console.log(res.data);
+    $http.get('http://192.168.0.109:8000/api/chironx/list/diagnosis', config).success(function (res) {
+        // console.log(res.data);
         $scope.listDiagnosis = res.data;
     });
 
@@ -92,6 +93,8 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
 
     // $scope.image_src = "https://chironzeta.s3-ap-south-1.amazonaws.com/output0.15169263900178676.fin.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ3F653OXC3NUQYSA%2F20180323%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20180323T131141Z&X-Amz-Expires=10000&X-Amz-SignedHeaders=host&X-Amz-Signature=2a80ce7c364aa80aa9aa3883af7747046f741a1c1bafd5c7b46b74ef0752e713";
     // $scope.image_src = "assets/img/test_image.jpg";
+
+    getRandomImage();
 
     $scope.min = 50;
     $scope.max = 200;
@@ -104,7 +107,7 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
     $scope.disabledStage_1 = false;
     $scope.prevDisabled = true;
     $scope.nextDisabled = true;
-    
+
     $scope.counter = 0;
     $scope.maxReached = 0;
 
@@ -121,17 +124,6 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
         $(".iv-large-image").css("-moz-filter", "none");
 
     };
-
-    $scope.nextRandomImage = function () {
-
-        $http.get('http://ec2-13-126-237-55.ap-south-1.compute.amazonaws.com:3000/api/chironx/status').success(function (res) {
-            console.log(res);
-            console.log("Pressed No");
-
-            $scope.image_src = "assets/img/test_image.jpg";
-
-        })
-    }
 
     var clinicalFeatList = [];
 
@@ -161,8 +153,7 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
 
     data = {};
 
-    checkcounter = function()
-    {
+    checkcounter = function () {
         // Setting prev and next counters
         $scope.counter += 1;
         $scope.maxReached += 1;
@@ -171,74 +162,53 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
         console.log("Max - " + $scope.maxReached);
 
     }
-    
-    // Stage - 1 - Quality Diagnosable
-    $scope.setValueLevel1 = function(){
-        
-        $scope.quality = 'Good';
-         
-        // prev and next button enable/disable
-         $scope.disabledStage_1 = true;
-         $scope.prevDisabled = false;
 
-         // Setting prev and next counters
-         checkcounter();
-        
+    // Stage - 1 - Quality Diagnosable
+    $scope.setValueLevel1 = function () {
+
+        $scope.quality = 'Diagnosable';
+
+        // prev and next button enable/disable
+        $scope.disabledStage_1 = true;
+        $scope.prevDisabled = false;
+
+        // Setting prev and next counters
+        checkcounter();
+
+        $scope.html = $compile('<li ng-repeat="diagnosis in listDiagnosis track by $index"><a>{{diagnosis}}</a></li>')($scope);
+        document.getElementById('diaglist').appendChild($scope.html[0]);
+
     }
 
     // Stage - 1 - Quality Bad/Non Diagnosable
-    $scope.setValueBadLevel1 = function(){
-        
+    $scope.setValueBadLevel1 = function () {
+
         $scope.quality = 'Non Diagnosable';
 
         data = {
-            image : $scope.imageObject,
-            quality : $scope.quality
+            image: $scope.imageObject,
+            quality: $scope.quality
         }
-        
-        $http.post('http://192.168.0.109:8000/api/chironx/markBad/assign/random/image', data,config).success(function (res) {
+
+        $http.post('http://192.168.0.109:8000/api/chironx/markBad/assign/random/image', data, config).success(function (res) {
             console.log(res);
 
-            if(res.image === 0)
-        {
-            growl.success('All current images have been marked!',{title: 'Success'});
-            $("#test *").attr("disabled", "disabled").off('click');
-
-            setDefaultImage();
-           
-        }
-        else
-        {
-            $scope.imageObject = res.image;
-            // $scope.image_src = "assets/img/test_image.jpg";
-            console.log("Done loading");
-            $scope.image_src = res.presignedUrl;
-    
-            var images=[];
-            var imageArr={};
-        
-                  imageArr={
-                    small : res.presignedUrl,
-                    big : res.presignedUrl
-        
-                  };
-                 images.push(imageArr);
-        
-              console.log(images);
-              imageView(images);
-        }
-
-
-
+            if(res.message === "Image Label Updated")
+            {
+                getRandomImage();
+            }
+            else{
+                growl.error(res.message, { title: 'Failure' });
+            }
         });
 
-        // // prev and next button enable/disable
+        // prev and next button enable/disable
         //  $scope.disabledStage_1 = true;
         //  $scope.prevDisabled = false;
 
         //  // Setting prev and next counters
         //  checkcounter();
-         
+
     }
 
 
@@ -271,38 +241,71 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
         if ($scope.Camera_Artefact) {
             $scope.qualityIssuesArr.push("Camera Artefact");
         }
-            console.log($scope.qualityIssuesArr);
-            
-            // Disable Checkboxes 
-            $("#disabledStage_2 *").attr("disabled", "disabled").off('click');
-            $scope.prevDisabled = false;
+        console.log($scope.qualityIssuesArr);
 
-            // Setting prev and next counters
-            checkcounter();
+        // Disable Checkboxes 
+        $("#disabledStage_2 *").attr("disabled", "disabled").off('click');
+        $scope.prevDisabled = false;
+
+        // Setting prev and next counters
+        checkcounter();
 
     }
 
     // Stage - 3 - Clinical features present/normal image
-    $scope.setValueLevel3 = function(value){
-        
+    $scope.setValueLevel3 = function (value) {
+
         $scope.clinicalFeat = []
-        
-        if (value === 'absent'){
+
+        if (value === 'absent') {
             $scope.clinicalFeat.push('Normal Image');
 
-            // NEXT RANDOM IMAGE CODE
-            // ---
-            // ---
+            data = {
+                image: $scope.imageObject,
+                quality: $scope.quality,
+                qualityIssues: $scope.qualityIssuesArr,
+                clinicalFeatures: $scope.clinicalFeat
+            }
+
+            console.log(data);
+
+            $http.post('http://192.168.0.109:8000/api/chironx/save/partialResult', data, config).success(function (res) {
+                console.log(res);
+    
+                if(res.message === "Stage 3 Updated")
+                {
+                    $window.location.reload();
+
+                    // // NEXT RANDOM IMAGE CODE
+                    // getRandomImage();
+
+                    // // SET COUNTERS
+                    // $('#disabledStage_2').find('*').prop('disabled',false);
+                    
+                    // $scope.Other = "";
+                    // $scope.disabledStage_1 = false;
+                    // $scope.counter = 0;
+                    // $scope.maxReached = 0;
+
+                    // $scope.prevDisabled = true;
+                    // $scope.nextDisabled = true;
+                }
+                else{
+                    growl.error(res.message, { title: 'Failure' });
+                }
+            });
 
         }
-        else{
+        else {
             // Setting prev and next counters
             checkcounter();
+            $scope.disabledStage_3 = true;
+            $scope.prevDisabled = false;
         }
 
         // prev and next button enable/disable
-        $scope.disabledStage_3 = true;
-        $scope.prevDisabled = false;
+        // $scope.disabledStage_3 = true;
+        // $scope.prevDisabled = false;
 
         console.log("counter - " + $scope.counter);
         console.log("Max - " + $scope.maxReached);
@@ -314,48 +317,116 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
     $scope.yes = [];
     $scope.no = [];
     $scope.unclear = [];
-    
-    $scope.setValueLevel4 = function(value,state,counter){
 
+    $scope.setValueLevel4 = function (value, state, counter) {
 
-        console.log(counter,"cunt")
+        console.log(counter, "cunt")
 
-        if(state === 'Present'){
+        if (state === 'Present') {
             $scope.clinicalFeat.push(value);
             $scope.yes.push(value);
         }
-        else if(state === 'Absent'){
+        else if (state === 'Absent') {
             // $scope.clinicalFeat.push(value);
             $scope.no.push(value);
         }
-        else{
+        else {
             // $scope.clinicalFeat.push(value);
             $scope.unclear.push(value);
         }
-        
-        if(counter){
-        console.log("disabledStage_Feat"+counter.toString())
-         // prev and next button enable/disable
-         $scope["disabledStage_Feat"+counter.toString()] = true;
-         $scope.prevDisabled = false;
+
+        if (counter) {
+            console.log("disabledStage_Feat" + counter.toString())
+            // prev and next button enable/disable
+            $scope["disabledStage_Feat" + counter.toString()] = true;
+            $scope.prevDisabled = false;
         }
-        else{
-         // prev and next button enable/disable
-         $scope.disabledStage_4 = true;
-         $scope.prevDisabled = false;
+        else {
+            // prev and next button enable/disable
+            $scope.disabledStage_4 = true;
+            $scope.prevDisabled = false;
         }
         // Setting prev and next counters
         checkcounter();
 
         data = {
-            quality : $scope.quality,
-            qualityIssues : $scope.qualityIssuesArr ,
-            clinicalFeatures : $scope.clinicalFeat,
-            yes : $scope.yes,
-            no : $scope.no,
-            unclear : $scope.unclear
+            quality: $scope.quality,
+            qualityIssues: $scope.qualityIssuesArr,
+            clinicalFeatures: $scope.clinicalFeat,
+            yes: $scope.yes,
+            no: $scope.no,
+            unclear: $scope.unclear
         }
         console.log(data);
+
+        if(counter === 16)
+        {
+            var DIV = document.getElementById("pevNextDiv");
+            DIV.style.display = "none";
+
+            var SUBDIV = document.getElementById("submitDiv");
+            SUBDIV.style.display = "block";
+        }
+
+    }
+
+    // Stage 5 Diagnosis
+    $scope.stage5SaveAllDetail = function(diagnosisList){
+
+        console.log($window.diagnosisList);
+        data = {
+            image: $scope.imageObject,
+            quality: $scope.quality,
+            qualityIssues: $scope.qualityIssuesArr,
+            clinicalFeatures: $scope.clinicalFeat,
+            yes: $scope.yes,
+            no: $scope.no,
+            unclear: $scope.unclear,
+            diagList: $window.diagnosisList
+        }
+
+        $http.post('http://192.168.0.109:8000/api/chironx/save/allResult', data, config).success(function (res) {
+                console.log(res);
+    
+                if(res.message === "All Results Saved")
+                {
+                    // // NEXT RANDOM IMAGE CODE
+                    // getRandomImage();
+
+                    $window.location.reload();
+
+                    // // SET COUNTERS
+                    // $('#disabledStage_2').find('*').prop('disabled',false);
+
+                    // $scope.Other = "";
+                    // $scope.disabledStage_1 = false;
+                    // $scope.disabledStage_3 = false;
+                    // $scope.disabledStage_4 = false;
+                    // $scope.counter = 0;
+                    // $scope.maxReached = 0;
+
+                    // for(count=0;count<=16;count++)
+                    // {
+                    //     console.log("disabledStage_Feat" + count.toString());
+                    //     $scope["disabledStage_Feat" + count.toString()] = false;
+                    // }
+                    
+                    var DIV = document.getElementById("pevNextDiv");
+                    DIV.style.display = "block";
+
+                    var SUBDIV = document.getElementById("submitDiv");
+                    SUBDIV.style.display = "none";
+
+                    // $scope.prevDisabled = true;
+                    // $scope.nextDisabled = true;
+                }
+                else{
+                    growl.error(res.message, { title: 'Failure' });
+                }
+            });
+
+        
+
 
     }
 
@@ -403,6 +474,7 @@ mainPhoneGapApp.controller('imageLoaderController', ['$scope', '$http', '$localS
             console.log("Max - " + $scope.maxReached);
         }
     };
+    
 
 
 }]);
